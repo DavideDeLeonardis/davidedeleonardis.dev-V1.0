89175cf4d8ee236ac9420eda65c992d02c09f287
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-08-04 22:29:36 +0100

    commit

--- src/components/Repositories/RepoItem.jsx
+++ src/components/Repositories/RepoItem.jsx
@@ -1,7 +1,7 @@
 import { links } from "../../assets/config/linksHrefRepos";
 
 const RepoItem = ({ repo }) => {
-   const setLinkHref = () => {
+   const getLinkHref = () => {
       for (let index = 0; index < links.length; index++) {
          const element = links[index];
          if (element.name === repo.name) {
@@ -10,13 +10,36 @@ const RepoItem = ({ repo }) => {
       }
    };
 
+   const getColor = () => {
+      switch (repo.language) {
+         case "HTML":
+            return `#E34C26`;
+         case "CSS":
+            return `#563D7C`;
+         case "JavaScript":
+            return `#F0E159`;
+         case "Vue":
+            return `#40B983`;
+         case "PHP":
+            return `#4F5E96`;
+         case "C#":
+            return `#188601`;
+         case "Solidity":
+            return `#AA6746`;
+         default:
+            return "";
+      }
+   };
+
    return (
       <li>
          - {repo.name}
          <br /> {repo.description}
          <br /> {repo.language}
-         <br />{" "}
-         <a href={setLinkHref()} target="_blank" rel="noreferrer">
+         <br />
+         <span className="color" style={{ backgroundColor: getColor() }}></span>
+         <br />
+         <a href={getLinkHref()} target="_blank" rel="noreferrer">
             See Demo
          </a>
          <br />
