fe308f2e4433458af7dd4e8de907c18725cd46d9
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-08-06 23:18:13 +0100

    commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,46 @@
+import { Fragment, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
-   return <div>Contact Me</div>;
+   const [message, setMessage] = useState(null);
+   const [isLoading, setIsLoading] = useState(false);
+   const form = useRef();
+
+   const sendEmail = (e) => {
+      e.preventDefault();
+      setIsLoading(true);
+
+      emailjs
+         .sendForm(
+            "service_jlf0fj6",
+            "template_84b8ugn",
+            form.current,
+            "3xD2auX5eX49KmlW8"
+         )
+         .then(() => {
+            setMessage("Message sent!");
+            setIsLoading(false);
+         })
+         .catch(() => {
+            setMessage("Something went wrong");
+            setIsLoading(false);
+         });
+   };
+
+   return (
+      <Fragment>
+         <h3>{message}</h3>
+         <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value={isLoading ? "Sending..." : "Send"} />
+         </form>
+      </Fragment>
+   );
 };
 
 export default Contact;
