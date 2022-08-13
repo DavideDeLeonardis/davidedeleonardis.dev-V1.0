608fa0e3baa50df65405de30f8553d56302deb6b
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-08-13 18:58:11 +0100

    ignore personal website

--- src/assets/config/ignoredRepos.js
+++ src/assets/config/ignoredRepos.js
@@ -1,4 +1,4 @@
-export const PERSONAL_WEBSITE_NAME = 'davidedeleonardis.dev';
+export const PERSONAL_WEBSITE_NAME = 'davidedeleonardis.dev-V1.0';
 export const PROJECT_1 = 'deliveboo';
 export const PROJECT_2 = 'react-amazon';
 export const PROJECT_3 = 'vue-netflix';
