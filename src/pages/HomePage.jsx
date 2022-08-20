b4778206fd7f1f4abdb6726ef0118f0960949433
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-08-20 15:50:33 +0100

    sizes for textarea

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import Contact from '../components/Contact';
 const HomePage = () => {
    return (
       <Layout>
-         {/* <About /> */}
+         <About />
          {/* <Projects /> */}
          <Contact />
       </Layout>
