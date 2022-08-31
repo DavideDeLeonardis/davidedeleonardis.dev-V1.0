63cfde3d10848a8d9bad374d2836197c26995cf6
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-08-31 18:00:36 +0100

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import '../assets/scss/index.scss';
 const App = () => {
    const [showHome, setShowHome] = useState(false);
 
-   setTimeout(() => setShowHome(true), 3800);
+   setTimeout(() => setShowHome(true), 0); // 3800
 
    return (
       <Routes>
