c74cd811943a695c868aad7cf34fded7e3ca3f5f
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-09-01 22:41:46 +0100

    fixes

--- src/components/About/Skills/index.jsx
+++ src/components/About/Skills/index.jsx
@@ -1,7 +1,14 @@
+import Slider3d from '../Slider-3d';
+
 import classes from './index.module.scss';
 
 const Skills = () => {
-   return <div className={classes.skills}>SKILLS</div>;
+   return (
+      <div className={classes.skills}>
+         <h1>SKILLS</h1>
+			<Slider3d />
+      </div>
+   );
 };
 
 export default Skills;
--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,5 +1,4 @@
 import Skills from './Skills';
-import Slider3d from './Slider-3d';
 import Heading from '../UI/Heading';
 
 import classes from './index.module.scss';
@@ -8,7 +7,6 @@ const About = () => {
    return (
       <section id="about" className={classes.about}>
          <Heading heading={'About Me'} h1ClassName={classes.h1Main} />
-			<Slider3d />
          <Skills />
       </section>
    );
