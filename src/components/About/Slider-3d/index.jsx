1005a91cdf7b581850a65aa006af0352a8591b36
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-09-04 17:38:15 +0200

    fix slider-3d, divide style files

--- src/components/About/Slider-3d/index.jsx
+++ src/components/About/Slider-3d/index.jsx
@@ -3,26 +3,27 @@ import { skills } from '../../../assets/config/skills';
 import classes from './index.module.scss';
 
 const Slider3d = () => {
-   const getImages = () => {
-      let index = 0;
-      while (index <= skills.length) {
-         const imagesList = skills.map((skill, i) => (
-            <div key={i} className={classes.skill} style={{ '--i': i }}>
-               <img
-                  className={classes[skill.class]}
-                  src={skill.image}
-                  alt={skill.name}
-               />
-               <span>{skill.name}</span>
-            </div>
-         ));
-         index++;
+   const imagesList = skills.map((skill, index) => (
+      <div
+         key={index}
+         className={classes.skill}
+         style={{
+            transform: `
+					rotateY(calc(${index} * calc(360deg / ${skills.length}))) 
+					translateZ(var(--width-skills-slider))
+				`,
+         }}
+      >
+         <img
+            className={classes[skill.class]}
+            src={skill.image}
+            alt={skill.name}
+         />
+         <span>{skill.name}</span>
+      </div>
+   ));
 
-         return imagesList;
-      }
-   };
-
-   return <div className={classes.gallery}>{getImages()}</div>;
+   return <div className={classes.gallery}>{imagesList}</div>;
 };
 
 export default Slider3d;
