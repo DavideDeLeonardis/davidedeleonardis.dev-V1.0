ba2d7ed0e3626324708dec3d66b6bc9b87662a36
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-09-13 21:48:05 +0200

    fix footer layout

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,14 +1,19 @@
 import dateFormat from 'dateformat';
 
+import useDimensions from '../../../hooks/useDimensions';
+
 import classes from './index.module.scss';
 
 const Footer = () => {
+   const { screenWidth } = useDimensions();
+	
    const date = dateFormat(new Date(), 'mmmm yyyy');
 
    return (
       <footer>
          <div className={classes.top}>
             Made by <span className={classes.name}>Davide De Leonardis</span> |
+            {screenWidth < 500 && <br />}
             <a
                className={classes.code}
                href="https://github.com/DavideDeLeonardis/davidedeleonardis.dev-V1.0"
