d5979831daa87f04886a0650d60d3e95988e3bae
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2023-02-01 08:13:19 +0100

    create component for project info

---
+++ src/components/Projects/ProjectItem/ProjectsInfo/index.jsx
@@ -0,0 +1,91 @@
+// ALL PROJECTS INFO
+
+
+// Libraries
+import { Trans } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// UI
+import Button from '../../../ui/Button';
+import LinkWithScrollOnTop from '../../../ui/LinkWithScrollOnTop';
+import SeeDemoGitHubButtons from '../../../ui/SeeDemoGitHubButtons';
+
+// SCSS
+import classes from '../../index.module.scss';
+
+const ProjectsInfo = ({
+   project,
+   hasMainStyle,
+   hideDetailsHandler,
+   displayMaps,
+	t
+}) => {
+
+   // Display "show details" button
+   const showDetailsButton = (
+      <Button>
+         {t('projects.show_details')}
+         <FontAwesomeIcon
+            className={classes.icon}
+            icon="fa-solid fa-arrow-up-right-from-square"
+         />
+      </Button>
+   );
+
+   return (
+      <div className={classes['info-container']}>
+         {!hasMainStyle && <h2>{project.name}</h2>}
+
+         {/* Description */}
+         <p>
+            <Trans
+               components={{
+                  technicals: <span className={classes['d-technicals']} />,
+                  blue: <span className="blue" />,
+               }}
+            >
+               {project.description}
+            </Trans>
+         </p>
+
+         {/* Topics */}
+         <ul className={classes['topic-container']}>
+            {displayMaps('topics', classes.topic)}
+         </ul>
+
+         {/* Project buttons */}
+         <div className={classes['project-bottom']}>
+            {hasMainStyle || project.hasDetailsPage ? (
+               // Show details page button
+               <div className={classes['project-links']}>
+                  <LinkWithScrollOnTop
+                     endpoint={`/projects/${project.name}`}
+                     state={project}
+                     content={showDetailsButton}
+                  />
+               </div>
+            ) : (
+               <div className={classes['project-links']}>
+                  {/* Buttons see demo and see gitHub */}
+                  <SeeDemoGitHubButtons
+                     project={project}
+                     className={classes.icon}
+                  />
+               </div>
+            )}
+
+            {!hasMainStyle && (
+               // Close details portal other project button
+               <button onClick={hideDetailsHandler}>
+                  <FontAwesomeIcon
+                     className={classes['info-close']}
+                     icon="fa-solid fa-xmark"
+                  />
+               </button>
+            )}
+         </div>
+      </div>
+   );
+};
+
+export default ProjectsInfo;
--- src/components/Projects/ProjectItem/index.jsx
+++ src/components/Projects/ProjectItem/index.jsx
@@ -1,7 +1,9 @@
 // Libraries
 import { useEffect, useState } from 'react';
-import { Trans, useTranslation } from 'react-i18next';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useTranslation } from 'react-i18next';
+
+// Components
+import ProjectsInfo from './ProjectsInfo';
 
 // Custom hooks
 import useFadeOnScroll from '../../../hooks/useFadeOnScroll';
@@ -9,8 +11,6 @@ import useFadeOnScroll from '../../../hooks/useFadeOnScroll';
 // UI
 import CardPortal from '../../ui/CardPortal';
 import Button from '../../ui/Button';
-import LinkWithScrollOnTop from '../../ui/LinkWithScrollOnTop';
-import SeeDemoGitHubButtons from '../../ui/SeeDemoGitHubButtons';
 
 // Assets
 import default_image from '../../../assets/images/default.png';
@@ -61,73 +61,6 @@ const ProjectItem = ({ project, hasMainStyle }) => {
       ));
    };
 
-   // Display "show details" button
-   const showDetailsButton = (
-      <Button>
-         {t('projects.show_details')}
-         <FontAwesomeIcon
-            className={classes.icon}
-            icon="fa-solid fa-arrow-up-right-from-square"
-         />
-      </Button>
-   );
-
-   // All projects' info
-   const projectInfo = (
-      <div className={classes['info-container']}>
-         {!hasMainStyle && <h2>{project.name}</h2>}
-
-         {/* Description */}
-         <p>
-            <Trans
-               components={{
-                  technicals: <span className={classes['d-technicals']} />,
-                  blue: <span className="blue" />,
-               }}
-            >
-               {project.description}
-            </Trans>
-         </p>
-
-         {/* Topics */}
-         <ul className={classes['topic-container']}>
-            {displayMaps('topics', classes.topic)}
-         </ul>
-
-         {/* Project buttons */}
-         <div className={classes['project-bottom']}>
-            {hasMainStyle || project.hasDetailsPage ? (
-               // Show details page button
-               <div className={classes['project-links']}>
-                  <LinkWithScrollOnTop
-                     endpoint={`/projects/${project.name}`}
-                     state={project}
-                     content={showDetailsButton}
-                  />
-               </div>
-            ) : (
-               <div className={classes['project-links']}>
-                  {/* Buttons see demo and see gitHub */}
-                  <SeeDemoGitHubButtons
-                     project={project}
-                     className={classes.icon}
-                  />
-               </div>
-            )}
-
-            {!hasMainStyle && (
-               // Close details portal other project button
-               <button onClick={hideDetailsHandler}>
-                  <FontAwesomeIcon
-                     className={classes['info-close']}
-                     icon="fa-solid fa-xmark"
-                  />
-               </button>
-            )}
-         </div>
-      </div>
-   );
-
    // Background position other projects details
    const getPosition = () => {
       if (
@@ -153,7 +86,14 @@ const ProjectItem = ({ project, hasMainStyle }) => {
                   backgroundPosition: getPosition(),
                }}
             ></div>
-            {projectInfo}
+
+            <ProjectsInfo
+               project={project}
+               hasMainStyle={hasMainStyle}
+               hideDetailsHandler={hideDetailsHandler}
+               displayMaps={displayMaps}
+               t={t}
+            />
          </div>
       </CardPortal>
    );
@@ -198,7 +138,13 @@ const ProjectItem = ({ project, hasMainStyle }) => {
                         </span>
                         {languages}
                      </div>
-                     {projectInfo}
+                     <ProjectsInfo
+                        project={project}
+                        hasMainStyle={hasMainStyle}
+                        hideDetailsHandler={hideDetailsHandler}
+                        displayMaps={displayMaps}
+                        t={t}
+                     />
                   </>
                ) : (
                   <>
